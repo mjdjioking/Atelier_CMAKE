8e3b4b1528da92860c3e2efd2e2b62c717828d74
mjdjioking <jiovani3009@gmail.com>
2026-04-02 13:00:19 +0200

    Add square function declaration to calculator.h

--- include/calculator.h
+++ include/calculator.h
@@ -1,4 +1,11 @@
+#ifndef CALCULATOR_H
+#define CALCULATOR_H
+
 double _add(double a, double b);
 double _sub(double a, double b);
 double _mul(double a, double b);
 double _div(double a, double b);
+// Nouvelle fonction
+double _square(double a); 
+
+#endif
